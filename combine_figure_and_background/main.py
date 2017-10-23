73e709e8d27d53258ed0624861bc7811463f837e
ZhengPeng <15732115701@163.com>
2017-10-23 20:37:46 +0800

    Speed up to 1 frame per second.

--- combine_figure_and_background/main.py
+++ combine_figure_and_background/main.py
@@ -1,26 +1,35 @@
 import cv2
 import numpy as np
-
+import time
 
 # get captures
 cap = cv2.VideoCapture(0)
 background_capture = cv2.VideoCapture(r'./a.avi')
 
+counter = -1
 while cap.isOpened():
-
+    counter += 1
+    start_time_extract_figure = time.time()
+    # your code
     # extract your figure
-    # refered from https://docs.opencv.org/trunk/d8/d83/tutorial_py_grabcut.html
     _, frame = cap.read()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     mask = np.zeros(frame.shape[:2], np.uint8)
     bgdModel = np.zeros((1, 65), np.float64)
     fgdModel = np.zeros((1, 65), np.float64)
-    rect = (10, 10, 450, 490)
-    cv2.grabCut(frame, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
+    rect = (200, 50, 300, 400)
+    start_time_grabCut = time.time()
+    cv2.grabCut(frame, mask, rect, bgdModel, fgdModel, 1, cv2.GC_INIT_WITH_RECT)
+    during_time_grabCut = time.time() - start_time_grabCut
+    print('{}-th t_time: {}'.format(counter, during_time_grabCut))
     mask2 = np.where((mask == 2) | (mask == 0), (0,), (1,)).astype('uint8')
     frame = frame * mask2[:, :, np.newaxis]
+    elapsed_time_extract_figure = time.time() - start_time_extract_figure
+    print('{}-th extract_figure_time: {}'.format(counter, elapsed_time_extract_figure))
 
     # extract the background
+    start_time_combination = time.time()
+    # your code
     ret, background = background_capture.read()
     background = cv2.resize(background, (640, 480), interpolation=cv2.INTER_AREA)
     # maybe the default size of embedded camera is 640x480
@@ -29,6 +38,8 @@ while cap.isOpened():
     mask_1 = frame > 0
     mask_2 = frame <= 0
     combination = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) * mask_1 + background * mask_2
+    elapsed_time_combination = time.time() - start_time_combination
+    print('{}-th combination_time: {}'.format(counter, elapsed_time_combination))
 
     cv2.imshow('combination', combination)
 
